54b312f9b5fdf845a9edf640c29d77476c2a2408
vfdxvffd <2201986113@qq.com>
2021-03-14 16:59:19 +0800

    引入单元测试依赖，增加单元测试

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,3 +1,7 @@
+import ioc.applicationContext.impl.SummerAnnotationConfigApplicationContext;
+import ioc.exception.DataConversionException;
+import ioc.test.Student;
+
 /**
  * @PackageName: PACKAGE_NAME
  * @ClassName: Test
@@ -6,4 +10,15 @@
  * @date: 2021/3/14 下午3:14
  */
 public class Test {
+
+    @org.junit.Test
+    public void test() {
+        try {
+            SummerAnnotationConfigApplicationContext ioc = new SummerAnnotationConfigApplicationContext("ioc.test");
+            Object bean = ioc.getBean(Student.class);
+            System.out.println(bean);
+        } catch (DataConversionException e) {
+            e.printStackTrace();
+        }
+    }
 }
