1044655bcc2bc1569b91c66a0385c1319bbf4c2b
vfdxvffd <2201986113@qq.com>
2021-03-21 19:32:56 +0800

    [增加] 增加了一种切入的方式

--- src/main/java/com/vfd/summer/aop/annotation/AfterReturning.java
+++ src/main/java/com/vfd/summer/aop/annotation/AfterReturning.java
@@ -1,2 +1,12 @@
-package com.vfd.summer.aop.annotation;public @interface AfterReturning {
+package com.vfd.summer.aop.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AfterReturning {
+    String value();
 }
